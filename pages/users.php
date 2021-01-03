86c5f25e6fb7956e7d61f2c77c985c9716deb73f
lowag <a.veres74@gmail.com>
2021-01-03 15:09:36 +0100

    fix

--- pages/users.php
+++ pages/users.php
@@ -11,6 +11,7 @@ $SQL.=" user_level=".(int) $_POST['user_level'].",";
 $SQL.=" user_parent_id=".(int) $_POST['user_parent_id'].",";
 $SQL.=" lang='".$dba->escapeStr($_POST['user_lang'])."',";
 $SQL.=" user_created=NOW();";
+$SQL.=" WHERE user_id=".$_SESSION['user_id'];
 
 
 if ($dba->Query($SQL)){
