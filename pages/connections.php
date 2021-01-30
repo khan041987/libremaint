23b71e00747e1b27b9f69f0fc5869b8f6fce31bb
lowag <a.veres74@gmail.com>
2021-01-30 15:53:37 +0100

    can modify a connection

--- pages/connections.php
+++ pages/connections.php
@@ -1,14 +1,29 @@
 
 <?php
-if (isset($_POST['connection_name_en']) && is_it_valid_submit() && isset($_SESSION['ADD_NEW_CONNECTION_TYPE'])){
+if (isset($_POST['new']) && is_it_valid_submit() && isset($_SESSION['ADD_NEW_CONNECTION_TYPE'])){
 
-$SQL="INSERT INTO connections (connection_name_en,connection_name_".$lang.",connection_type,connection_category_id,connection_review_en,connection_review_".$lang.") VALUES ";
-$SQL.="('".$dba->escapeStr($_POST['connection_name_en'])."',";
-$SQL.="'".$dba->escapeStr($_POST['connection_name_'.$lang])."',";
+$SQL="INSERT INTO connections (";
+
+if (isset($_SESSION['CAN_WRITE_LANG1']))
+    $SQL.="connection_name_".LANG1.",connection_review_".LANG1.",";
+
+if (LANG2_AS_SECOND_LANG && isset($_SESSION['CAN_WRITE_LANG2']))
+    $SQL.="connection_name_".LANG2.",connection_review_".LANG2.",";
+    
+$SQL.="connection_type,connection_category_id) VALUES (";
+
+if (isset($_SESSION['CAN_WRITE_LANG1'])){
+$SQL.="'".$dba->escapeStr($_POST['connection_name_'.LANG1])."',";
+$SQL.="'".$dba->escapeStr($_POST['connection_review_'.LANG1])."',";
+}
+
+if (LANG2_AS_SECOND_LANG && isset($_SESSION['CAN_WRITE_LANG2'])){
+$SQL.="'".$dba->escapeStr($_POST['connection_name_'.LANG2])."',";
+$SQL.="'".$dba->escapeStr($_POST['connection_review_'.LANG2])."'";
+}
+
 $SQL.=(int) $_POST['connection_type'].",";
 $SQL.=(int) $_POST['connection_category_id'].",";
-$SQL.="'".$dba->escapeStr($_POST['connection_review_en'])."',";
-$SQL.="'".$dba->escapeStr($_POST['connection_review_'.$lang])."'";
 $SQL.=")";
 
 
@@ -20,7 +35,41 @@ if (LM_DEBUG)
 error_log($SQL,0);
 }
 
-if (isset($_GET["new"]) ){
+
+if (isset($_POST['modify']) && isset($_POST['connection_id']) && is_it_valid_submit() && isset($_SESSION['ADD_NEW_CONNECTION_TYPE'])){
+
+$SQL="UPDATE connections SET ";
+
+if (isset($_SESSION['CAN_WRITE_LANG1'])){
+    $SQL.="connection_name_".LANG1."='".$dba->escapeStr($_POST['connection_name_'.LANG1])."',";
+    
+    $SQL.="connection_review_".LANG1."='".$dba->escapeStr($_POST['connection_review_'.LANG1])."',";
+}
+
+
+if (LANG2_AS_SECOND_LANG && isset($_SESSION['CAN_WRITE_LANG2']))
+{
+    $SQL.="connection_name_".LANG2."='".$dba->escapeStr($_POST['connection_name_'.LANG2])."',";
+    $SQL.="connection_review_".LANG2."='".$dba->escapeStr($_POST['connection_review_'.LANG2])."',";
+ }
+ 
+$SQL.="connection_type=".(int) $_POST['connection_type'].",";
+$SQL.="connection_category_id=".(int) $_POST['connection_category_id'];
+
+$SQL.=" WHERE connection_id=".(int) $_POST['connection_id'];
+
+if ($dba->Query($SQL))
+        lm_info(gettext("The connection has been modified."));
+                else
+        lm_info(gettext("Failed to modify connection.".$dba->err_msg));
+if (LM_DEBUG)
+error_log($SQL,0);
+}
+
+
+
+
+if (isset($_GET["new"]) || isset($_GET["modify"]) ){
 ?>
 <div class="card">
 <div class="card-header">
@@ -32,7 +81,13 @@ if (isset($_GET["new"]) ){
 <?php
 if (!isset($_SESSION['ADD_NEW_CONNECTION_TYPE']))
 lm_die(gettext("You have no permission!"));
-    
+if (isset($_GET["modify"]))
+{
+$SQL="SELECT * FROM connections WHERE connection_id=".(int) $_GET['connection_id'];
+$connection_row=$dba->getRow($SQL);
+}
+
+
 echo "<div class=\"row form-group\">\n";
         echo "<div class=\"col col-md-2\"><label for=\"connection_category_id\" class=\"form-control-label\">".gettext("Connection category:")."</label></div>\n";
         echo "<div class=\"col-8 col-md-6\">";
@@ -46,48 +101,83 @@ echo "<div class=\"row form-group\">\n";
         foreach ($result as $row)
         {
             echo "<option value=\"".$row["connection_category_id"]."\"";
-           
+           if (isset($_GET["modify"]) && $connection_row['connection_category_id']==$row["connection_category_id"])
+           echo " selected";
             echo ">".$row["connection_category_".$lang]."</option>\n";
         }
         echo "</select></div></div>";
 
+if ($_SESSION['CAN_WRITE_LANG1']){        
 echo "<div class=\"row form-group\">";
-echo "<div class=\"col col-md-2\"><label for=\"connection_name_en\" class=\"form-control-label\">".gettext("Connection name en:")."</label></div>\n";
-echo "<div class=\"col-8 col-md-6\"><input type=\"text\" id=\"connection_name_en\" name=\"connection_name_en\" placeholder=\"".gettext("connection name en")."\" class=\"form-control\" required></div>\n";
-echo "</div>";
+echo "<div class=\"col col-md-2\"><label for=\"connection_name_".LANG1."\" class=\"form-control-label\">".gettext("Connection name").LANG1.":</label></div>\n";
+echo "<div class=\"col-8 col-md-6\"><input type=\"text\" id=\"connection_name_".LANG1."\" name=\"connection_name_".LANG1."\" placeholder=\"".gettext("connection name")." ".LANG1."\" class=\"form-control\"";
 
-if ($lang!="en"){
+if (isset($_GET["modify"]))
+echo " VALUE=\"".$connection_row['connection_name_'.LANG1]."\"";
+
+echo " required></div>\n";
+echo "</div>";
+}
+
+
+if (isset($_SESSION['CAN_WRITE_LANG2']) && LANG2_AS_SECOND_LANG){
 echo "<div class=\"row form-group\">";
-echo "<div class=\"col col-md-2\"><label for=\"connection_name_".$lang."\" class=\"form-control-label\">".gettext("Connection name:")."</label></div>\n";
-echo "<div class=\"col-8 col-md-6\"><input type=\"text\" id=\"connection_name_".$lang."\" name=\"connection_name_".$lang."\" placeholder=\"".gettext("connection name")."\" class=\"form-control\" required></div>\n";
+echo "<div class=\"col col-md-2\"><label for=\"connection_name_".LANG2."\" class=\"form-control-label\">".gettext("Connection name:")."</label></div>\n";
+echo "<div class=\"col-8 col-md-6\"><input type=\"text\" id=\"connection_name_".LANG2."\" name=\"connection_name_".LANG2."\" placeholder=\"".gettext("connection name")."\" class=\"form-control\"";
+
+if (isset($_GET["modify"]))
+echo " VALUE=\"".$connection_row['connection_name_'.LANG2]."\"";
+
+echo " required></div>\n";
 echo "</div>";
 }
 echo "<div class=\"row form-group\">\n";
                 echo "<div class=\"col col-md-2\"><label for=\"connection_type\" class=\"form-control-label\">".gettext("Connection type:")."</label></div>\n";
                 echo "<div class=\"col col-md-2\">";
                 echo "<select name=\"connection_type\" id=\"connection_type\" class=\"form-control\">\n";
-                $i=1;
+                $i=0;
                 //$connection_types from lm-settings.php
-                foreach ($connection_types as $connection_type)
-                echo "<option value=\"".$i++."\">".$connection_type."</option>\n";
-                                
+                foreach ($connection_types as $connection_type){
+                echo "<option value=\"".++$i."\"";
+                if (isset($_GET["modify"]) && $connection_row['connection_type']==$connection_type)
+           echo " selected";
+                echo ">".$connection_type."</option>\n";
+                }                
                 echo "</select>";
                 echo "</div>\n";
 echo "</div>\n";
 
+if ($_SESSION['CAN_WRITE_LANG1']){
 echo "<div class=\"row form-group\">";
-echo "<div class=\"col col-md-2\"><label for=\"connection_review_en\" class=\"form-control-label\">".gettext("Connection review en:")."</label></div>\n";
-echo "<div class=\"col-8 col-md-6\"><input type=\"text\" id=\"connection_review_en\" name=\"connection_review_en\" placeholder=\"".gettext("connection review en")."\" class=\"form-control\"></div>\n";
-echo "</div>";
+echo "<div class=\"col col-md-2\"><label for=\"connection_review_".LANG1."\" class=\"form-control-label\">".gettext("Connection review").":</label></div>\n";
+echo "<div class=\"col-8 col-md-6\"><input type=\"text\" id=\"connection_review_".LANG1."\" name=\"connection_review_".LANG1."\" placeholder=\"".gettext("connection review")." ".LANG1."\" class=\"form-control\"";
+if (isset($_GET["modify"]))
+echo " VALUE=\"".$connection_row['connection_review_'.LANG1]."\"";
 
+echo "></div>\n";
+echo "</div>";
+}
+
+if (isset($_SESSION['CAN_WRITE_LANG2']) && LANG2_AS_SECOND_LANG){
 echo "<div class=\"row form-group\">";
-echo "<div class=\"col col-md-2\"><label for=\"connection_review_".$lang."\" class=\"form-control-label\">".gettext("Connection review:")."</label></div>\n";
-echo "<div class=\"col-8 col-md-6\"><input type=\"text\" id=\"connection_review_".$lang."\" name=\"connection_review_".$lang."\" placeholder=\"".gettext("connection review")."\" class=\"form-control\"></div>\n";
-echo "</div>";
+echo "<div class=\"col col-md-2\"><label for=\"connection_review_".LANG2."\" class=\"form-control-label\">".gettext("Connection review:")."</label></div>\n";
+echo "<div class=\"col-8 col-md-6\"><input type=\"text\" id=\"connection_review_".LANG2."\" name=\"connection_review_".LANG2."\" placeholder=\"".gettext("connection review")."\" class=\"form-control\"";
+if (isset($_GET["modify"]))
+echo " VALUE=\"".$connection_row['connection_review_'.LANG2]."\"";
 
+echo "></div>\n";
+echo "</div>";
+}
 
 echo "<INPUT TYPE=\"hidden\" name=\"page\" id=\"page\" value=\"connections\">";
 echo "<input type=\"hidden\" name=\"valid\" id=\"valid\" value=\"".$_SESSION["tit_id"]."\">";
+
+if (isset($_GET['new']))
+echo "<input type=\"hidden\" name=\"new\" id=\"new\" value=\"1\">";
+else if (isset($_GET['modify'])){
+echo "<input type=\"hidden\" name=\"modify\" id=\"modify\" value=\"1\">";
+echo "<input type=\"hidden\" name=\"connection_id\" id=\"connection_id\" value=\"".(int) $_GET['connection_id']."\">";
+}
 
 echo "<div class=\"card-footer\"><button type=\"submit\" class=\"btn btn-primary btn-sm\">\n";
 echo "<i class=\"fa fa-dot-circle-o\"></i> Submit </button>\n";
@@ -146,7 +236,13 @@ echo "<div class=\"user-area dropdown float-right\">\n";
                              if (isset($_SESSION["SEE_CONNECTION_OF_PRODUCT"]) && isset($_SESSION["SEE_CONNECTION_OF_ASSET"])){
                             echo "<a class=\"nav-link\" href=\"javascript:ajax_call('show_assets_products_with_this_connection','".$row['connection_id']."','','','','".URL."index.php','for_ajaxcall')\"><i class=\"fa fa-user\"></i> ";
                              echo gettext("Show assets, product with this connection")."</a>";}
-                       
+                             
+                             if (isset($_SESSION['ADD_NEW_CONNECTION_TYPE']))
+                            {
+                            echo "<a class=\"nav-link\" href=\"index.php?page=connections&connection_id=".$row['connection_id']."&modify=1\"><i class=\"fa fa-user\"></i> ";
+                             echo gettext("Modify connection")."</a>";
+                            
+                            }
                              echo "</div>";
     echo "</div>";
  
